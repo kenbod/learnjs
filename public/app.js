d1d93da48d5e6c83e935cedc06389421d31033a6
Ken Anderson <ken.anderson@colorado.edu>
2016-11-06 16:00:08 -0700

    Data Binding Added

--- public/app.js
+++ public/app.js
@@ -2,9 +2,40 @@
 
 var learnjs = {};
 
-learnjs.problemView = function(number) {
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  },
+  {
+    description: "Start of an Array",
+    code: "function problem() { list = [42, 23, 10]; return __ === 42; }"
+  },
+  {
+    description: "Accessing Attributues",
+    code: 'function problem() { p = {name: "Ken"}; return __ == "Ken"; }'
+  },
+  {
+    description: "Increment Variable",
+    code: 'function problem() { a = 41; return (++a == __);}'
+  }
+];
+
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+learnjs.problemView = function(data) {
+  var number = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
   view.find('.title').text('Problem #' + number);
+  learnjs.applyObject(learnjs.problems[number -1], view);
   return view;
 }
 
